4580ccd5e1139d42a83e00c2bb9c7b4268092aa8
Aislin Black <ablack17@newarka.edu>
2016-07-12 11:03:55 -0400

    Initial Commit

---
+++ Currency Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Currency Converter
+//
+//  Created by Aislin Philomena Black on 7/12/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Currency Converter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Currency Converter
+//
+//  Created by Aislin Philomena Black on 7/12/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
